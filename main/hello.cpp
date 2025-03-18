914a6c1146730cf02d8c3c03faf3036d2792ae78
ankit <ankit.avinash@gmail.com>
2025-03-18 15:18:05 +0530

    my commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,10 @@
-//File: hello.cpp
+// File: hello.cpp
 #include <iostream>
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "Hello, World" << endl;
+  cout << "Hello, Jenkins" << endl;
+  cout << "I have successfully built and run this" << endl;
   return 0;
 }
